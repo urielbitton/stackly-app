ddb2f15cf0385d8f7167cdcb685512257bfcbbf0
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2020-12-15 02:03:53 -0500

    Navbar UI

--- src/comps/Homecont.js
+++ src/comps/Homecont.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Home from './Home'
 
 function Homecont() {
   return (
     <div className="homecont">
       <Navbar />
+      <Home />
     </div>
   )
 } 
--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -2,9 +2,27 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <nav>
+      <div className="logocont">
+        <img src="https://i.imgur.com/wazsi0l.png" alt=""/>
+        <h5>Stackly.</h5>
+      </div>
 
-    </div>
+      <div className="toolbar">
+        <div className="searchbox">
+          <i className="far fa-search"></i>
+        </div>
+        <div className="msgbox">
+          <i className="far fa-comments-alt"></i>
+        </div>
+        <div className="notifbox">
+        <i className="far fa-bell"></i>
+        </div>
+        <div className="optionsbox">
+          <i className="fas fa-th"></i>
+        </div>
+      </div>
+    </nav>
   )
 }
 
