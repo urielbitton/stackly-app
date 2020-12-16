c838f78ffbc06f011c10fee4d0dfab2dceec3f65
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2020-12-15 19:02:09 -0500

    Home UI

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="home">
+    <div className="home apppage">
+      <div className="apptitles">
+        <h5>Overview</h5>
+        <h6>An overview of your projects, clients and tasks.</h6>
+      </div>
+
+      <div className="homegrid">
+        <div className="banner homebanner">
+          <div className="bannertext">
+            <h4>Good Morning, Uriel</h4>
+            <h6>Check your recent notifications and open tasks</h6>
+          </div>
+          <img src="https://i.imgur.com/iOYuiob.png" alt=""/>
+        </div>
+      </div>
 
     </div>
   )
