c387aca5f5e2117ae335ebff9048ba420c5f6e0e
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2020-12-18 01:39:40 -0500

    Finished Home UI

---
+++ src/comps/Chart.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useContext } from 'react'
+import Chart from 'chart.js';
+
+function Charts(props) { 
+
+  useEffect(() => {
+
+    //bar chart    
+    new Chart(document.getElementById("bar-chart"), {
+      type: 'bar',
+      data: {
+        labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+        datasets: [{ 
+            data: [1,5,7,8,4,3,4,5,6,9,3,4],
+            label: "Clients",
+            borderColor: "#fafafa",
+            fill: true,
+            backgroundColor: "#35b6fa"
+          }, { 
+            data: [1,5,7,8,4,3,4,5,6,9,3,4],
+            label: "Tasks",
+            borderColor: "#fafafa",
+            fill: true,
+            backgroundColor: "#f9cd0a"  
+          }, { 
+            data: [1,5,7,8,4,3,4,5,6,9,3,4],
+            label: "Projects",
+            borderColor: "#fafafa",
+            fill: true,
+            backgroundColor: "#b230fc"   
+          }
+        ]   
+      },      
+      options: { 
+        responsive:true,
+        maintainAspectRatio: false,
+        title: {
+          display: false,
+        },
+        scales:{
+          xAxes: [{
+            stacked: true,
+            barPercentage: 0.1, 
+            gridLines: {
+              color: '#f5f5f5'
+            },
+          }],
+          yAxes: [{
+            stacked: true,
+            gridLines: {
+              color: '#f5f5f5'
+            },
+          }]
+        }
+      }
+    });  
+    
+ 
+  },[])
+
+  return (
+    <>
+      <canvas id={props.type}></canvas>
+    </> 
+  )
+}
+
+export default Charts
--- src/comps/Home.js
+++ src/comps/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Calendars from './Calendars'
+import Charts from "./Chart"
 
 function Home() {
   return (
@@ -10,16 +11,40 @@ function Home() {
       </div>
 
       <div className="homegrid">
-        <div className="banner homebanner">
-          <div className="bannertext">
-            <h4>Good Morning, Uriel</h4>
-            <h6>Check your recent notifications and tasks</h6>
-            <div className="quicknotifs">
-              <small><i className="fal fa-bell"></i>3 Notifications</small>
-              <small><i className="fal fa-tasks"></i>5 Tasks</small>
+        <div className="homemain">
+          <div className="banner homebanner">
+            <div className="bannertext">
+              <h4>Good Morning, Uriel</h4>
+              <h6>Check your recent notifications and tasks</h6>
+              <div className="quicknotifs">
+                <small><i className="fal fa-bell"></i>3 Notifications</small>
+                <small><i className="fal fa-tasks"></i>5 New Tasks</small>
+              </div>
+            </div>
+            <img src="https://i.imgur.com/iOYuiob.png" alt=""/>
+          </div>
+          <div className="dashgraph dashbox">
+            <Charts type="bar-chart" /> 
+          </div>
+          <div className="dashbox tasksbox">
+            <h5>Latest Tasks</h5>
+            <div className="taskhead">
+              <h6>Task</h6>
+              <h6>Client</h6>
+              <h6>Stage</h6>
+              <h6>Date</h6>
+              <h6>Status</h6>
+              <h6>Details</h6>
+            </div>
+            <div className="taskrow">
+              <h6>Redesign Website</h6>
+              <h6>Cindy Bitton</h6>
+              <h6>Development</h6>
+              <h6>Jan 09 2021</h6>
+              <h6>Not Done</h6>
+              <h6><i class="far fa-ellipsis-h"></i></h6>
             </div>
           </div>
-          <img src="https://i.imgur.com/iOYuiob.png" alt=""/>
         </div>
         <div className="homeside">
           <Calendars />
@@ -44,6 +69,7 @@ function Home() {
             </div>
             <div className="view"><i className="far fa-angle-right"></i></div>
           </div>
+          <div className="updatesbox dashbox"></div>
         </div>
       </div>
 
